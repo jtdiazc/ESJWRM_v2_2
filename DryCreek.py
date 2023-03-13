e005cbf756f61c721423612dfc563fc34f0c640a
JDiaz <jdiaz@hydrofocus.com>
2023-03-13 13:58:39 -0700

    Plot velocity fields

--- DryCreek.py
+++ DryCreek.py
@@ -12,15 +12,15 @@ from moviepy.video.io.bindings import mplfig_to_npimage
 
 
 #Path to ESJWRM 2.2
-model_path=r"C:\Projects\5658\ESJWRM Version 2.2 modified"
+model_path=r"C:\Users\jdiaz\OneDrive - HydroFocus\5658_old\ESJWRM Version 2.2 modified"
 
 #Shapefiles output path
-shp_out=r"P:\Projects\5658_NSJWCD\GIS\Vector\Model Shapefiles\Streams"
+shp_out=r"C:\Users\jdiaz\OneDrive - HydroFocus\5658\ESJWRM_v2_2\Gain_from_GW\Vector"
 
 #Output path for csvs
-csv_out=r"P:\Projects\5658_NSJWCD\ESJWRM_v2_2\Mokelumne"
+csv_out=r"C:\Users\jdiaz\OneDrive - HydroFocus\5658\ESJWRM_v2_2\Gain_from_GW\CSV"
 
-dir_out=r"P:\Projects\5658_NSJWCD\ESJWRM_v2_2\Dry_Creek"
+dir_out=r"C:\Users\jdiaz\OneDrive - HydroFocus\5658\ESJWRM_v2_2\Gain_from_GW"
 
 
 #preprocessor file
@@ -29,9 +29,15 @@ preprocessor_in_file=os.path.join(model_path,r"Preprocessor\PreProcessor_MAIN.IN
 #simulation file
 simulation_in_file = os.path.join(model_path,r"Simulation\Simulation_MAIN.IN")
 
-# create instance of the IWFMModel class
-m = pywfm.IWFMModel(preprocessor_in_file, simulation_in_file)
 
-stream_name='Dry Creek'
-pyhf.utils.iwfm_gaining(m,stream_name)
+
+
+for stream_name in ['Mokelumne River','Dry Creek']:
+    # create instance of the IWFMModel class
+    m = pywfm.IWFMModel(preprocessor_in_file, simulation_in_file)
+    pyhf.utils.iwfm_gaining(m,stream_name,
+                            shp_out=shp_out,
+                            bounds=os.path.join(shp_out,"NSJWCD_26910.shp"),
+                            im_path=os.path.join(dir_out,"Background.png"),
+                            dir_out=dir_out)
 
